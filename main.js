d929f2d657e1a401cda38d6a3d3c230ac6e46a21
Harish0411 <hariharanramasamy98@gmail.com>
2023-03-10 19:22:30 +0530

    adding quiz-3

---
+++ main.js
@@ -0,0 +1,13 @@
+// 3)Identify all of the variables, object property names, primitive values, and objects shown in the following code
+
+function hello(greeting, name) {   // greeting & name are parameters of hello function
+  return greeting + ' ' + name;   //hello function returns arguments what we are passing
+}
+
+function xyzzy() {
+  return { a: 1, b: 2, c: [3, 4, 5], d: {} }; //xyzzy function returns an object that object values are in numbers,array,and also an object
+}
+
+const howdy = hello('Hi', 'Grace'); // stored hello function call in const keyword and Hi & Grace are two arguments pass to the hello function
+let foo = xyzzy(); // stored xyzzy function call in let keyword
+
